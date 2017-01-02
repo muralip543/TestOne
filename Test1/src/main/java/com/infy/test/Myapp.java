d3b3f098dd30d2a22c8431cc8f7742eec8906771
pallapu krishna <pallapu.krishna@newvoe.local>
2017-01-02 06:19:14 +0000

    Test1 commited

---
+++ Test1/src/main/java/com/infy/test/Myapp.java
@@ -0,0 +1,19 @@
+package com.infy.test;
+
+public class Myapp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Navya gaaru GM***");
+		System.out.println("Welcome to Dal Team***");
+		System.out.println("Welcome to Basket Team***");
+		
+	}
+public void sayMorning(){
+	
+	System.out.println("********");
+}
+public void saymorning(){
+	
+	System.out.println("********");
+}
+}
